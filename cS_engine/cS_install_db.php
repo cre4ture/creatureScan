cfb9d0e5713dd4d04a9307efbe6fa10d6e188ce9
creature <cre4ture@users.noreply.github.com>
2009-11-01 17:52:05 +0000

    this is for running the db_structure.sql

---
+++ cS_engine/cS_install_db.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+
+    Project: cS_engine
+    File: simpleXML-DOM - Interface
+    Author: Ulrich Hornung
+    Date: 05.08.2009
+    Type: function library (please use "require_once" instead of "include")
+    
+    Description:
+    
+    this file converts xml structures to "normal" cS_engine structures
+    and vice versa
+    
+*/
+
+// function libraries
+require_once "cS_sql.php";
+
+// get all $_POST data as single stream
+$data = file_get_contents('db_structure.sql');
+
+// connect to database
+cSsql_db_login();
+
+// execute sql
+mysql_unbuffered_query($data);
+
+// disconnect from database
+cSsql_db_logout();
+
+
+?>
