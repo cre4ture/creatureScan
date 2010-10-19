5f61538ec9b278fd63091d8e3686b283f5aa9f6c
creaturescan <creaturescan@27648461-5669-0410-9c1b-ee6ced466d99>
2010-10-19 21:19:58 +0000

    Release 1.9b

--- Prog_Unit.pas
+++ Prog_Unit.pas
@@ -40,7 +40,7 @@ const
   pi_TSyncRaids      = 1029;
   pi_TSyncStats      = 1030;
 
-const VNumber = '1.9a';
+const VNumber = '1.9b';
       
 
 {$DEFINE oanzahl}  //ohne Anzahl!! -- brauchts nirgends mehr!
