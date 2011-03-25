0eca5d993e086557a02a836da33967a03d5b9623
creaturescan <cre4ture@users.noreply.github.com>
2011-03-25 06:50:54 +0000

    new cshelper with TCP server working but running actually as client

--- cshelper/srcExtension/defaults/preferences/prefs.js
+++ cshelper/srcExtension/defaults/preferences/prefs.js
@@ -1,5 +1,8 @@
-/* Preferences sample:
-pref("cshelper.prefkey4String", "A string");
-pref("cshelper.prefkey4boolean", false);
-pref("cshelper.prefkey4number", 0);
-*/
+pref("cshelper.active", true);
+pref("cshelper.readGalaxy", true);
+pref("cshelper.readMessages", true);
+pref("cshelper.readStatistics", true);
+pref("cshelper.tcpip.enabled", true);
+pref("cshelper.tcpip.loopbackOnly", true);
+pref("cshelper.tcpip.backlog", 1);
+pref("cshelper.tcpip.port", 8185);
