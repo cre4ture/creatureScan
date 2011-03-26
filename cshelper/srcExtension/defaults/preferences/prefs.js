513af926ce91494096c8ee437fb469215efc78b7
creaturescan <cre4ture@users.noreply.github.com>
2011-03-26 19:09:12 +0000

    1.2

--- cshelper/srcExtension/defaults/preferences/prefs.js
+++ cshelper/srcExtension/defaults/preferences/prefs.js
@@ -1,8 +1,8 @@
-pref("cshelper.active", true);
-pref("cshelper.readGalaxy", true);
-pref("cshelper.readMessages", true);
-pref("cshelper.readStatistics", true);
-pref("cshelper.tcpip.enabled", true);
-pref("cshelper.tcpip.loopbackOnly", true);
-pref("cshelper.tcpip.backlog", 1);
-pref("cshelper.tcpip.port", 8185);
+//pref("cshelper.active", true);
+//pref("cshelper.readGalaxy", true);
+//pref("cshelper.readMessages", true);
+//pref("cshelper.readStatistics", true);
+//pref("cshelper.tcpip.enabled", true);
+//pref("cshelper.tcpip.loopbackOnly", true);
+//pref("cshelper.tcpip.backlog", 1);
+//pref("cshelper.tcpip.port", 8185);
